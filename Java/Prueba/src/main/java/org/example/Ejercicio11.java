ecfdb5146f1229abea43df58b7a4c0c0e30d5b6c
PablodelaRosa07 <prosgon2108@g.educaand.es>
2025-12-10 12:04:57 +0100

    subo

---
+++ Java/Prueba/src/main/java/org/example/Ejercicio11.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio11 {
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Introduce un número:");
+
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("El número mayor es:" + num1);
+
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("El número mayor es:" + num2);
+
+
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println("El número mayor es:" + num3);
+
+
+        }
+
+
+    }
+}
