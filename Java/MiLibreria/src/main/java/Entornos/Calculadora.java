eee5f763f601ec558d7ac77860d4c51195d5e366
PablodelaRosa07 <prosgon2108@g.educaand.es>
2026-01-12 09:02:19 +0100

    subo

---
+++ Java/MiLibreria/src/main/java/Entornos/Calculadora.java
@@ -0,0 +1,13 @@
+package Entornos;
+
+public class Calculadora {
+    public int suma (int num1, int num2)
+    {
+        return num1+num2;
+    }
+
+    public double suma (double num1, double num2)
+    {
+        return num1+num2;
+    }
+}
---
+++ Java/PruebaEntorno/src/main/java/GestionaCuentas.java
@@ -0,0 +1,11 @@
+import entornos.Calculadora;
+public class GestionaCuentas {
+    static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.suma(3,4);
+        System.out.println(resultado);
+
+    }
+}
