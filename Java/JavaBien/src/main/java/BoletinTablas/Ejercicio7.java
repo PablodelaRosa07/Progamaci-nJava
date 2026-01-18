e6ca92aea14990f4560ed61feab8801ab776d6d2
PablodelaRosa07 <prosgon2108@g.educaand.es>
2026-01-18 12:14:43 +0100

    subo

---
+++ Java/JavaBien/src/main/java/BoletinTablas/Ejercicio7.java
@@ -0,0 +1,22 @@
+package BoletinTablas;
+
+public class Ejercicio7 {
+    static void main(String[] args) {
+        int[] numeros = {4, 3, 5, 2, 7, 11, 8};
+        int contador = 0;
+
+    }
+
+    public int ordenarTabla(int[] numeros, int contador){
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (numeros[contador] > numeros[contador+1]){
+                    numeros.remove(contador+1)
+
+            }
+
+        }
+
+        }
+    }
+}
