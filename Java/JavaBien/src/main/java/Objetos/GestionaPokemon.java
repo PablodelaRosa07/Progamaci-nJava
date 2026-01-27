d4127e635d2003057825e95f0d4463c556315014
PablodelaRosa07 <prosgon2108@g.educaand.es>
2026-01-27 20:53:15 +0100

    subo

---
+++ Java/JavaBien/src/main/java/Objetos/GestionaPokemon.java
@@ -0,0 +1,13 @@
+package Objetos;
+
+public class GestionaPokemon {
+    static void main(String[] args) {
+
+        Pokemon Pikachu = new Persona();
+        Pikachu.nombre = "Pikachu";
+        Pikachu.tipo = "Eléctrico";
+        Pikachu.vida = "Cuenca";
+        Pikachu.evoluciones = "Avatar";
+
+    }
+}
