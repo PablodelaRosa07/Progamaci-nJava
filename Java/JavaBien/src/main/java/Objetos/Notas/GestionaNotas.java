1d75f37a59eff9c692b0032b66ac4520da428b3f
PablodelaRosa07 <prosgon2108@g.educaand.es>
2026-02-02 17:59:08 +0100

    subo

--- Java/JavaBien/src/main/java/Objetos/Notas/GestionaNotas.java
+++ Java/JavaBien/src/main/java/Objetos/Notas/GestionaNotas.java
@@ -1,4 +1,17 @@
 package Objetos.Notas;
 
 public class GestionaNotas {
+    static void main(String[] args) {
+
+        Notas persona1 = new Notas("Mates", 9, 7, 8, 8);
+        Notas persona2 = new Notas("Mates", 4, 5, 5, 5);
+        Notas persona3 = new Notas("Mates", 10, 10, 10, 10);
+
+        persona1.trimestresAprobados();
+        persona1.notaMediaFinal();
+        persona2.trimestresAprobados();
+        persona2.notaMediaFinal();
+        persona3.trimestresAprobados();
+        persona3.notaMediaFinal();
+    }
 }
--- Java/JavaBien/src/main/java/Objetos/Notas/Notas.java
+++ Java/JavaBien/src/main/java/Objetos/Notas/Notas.java
@@ -1,4 +1,37 @@
 package Objetos.Notas;
 
 public class Notas {
+
+    String nombreAsignatura;
+    int notaPrimerTrimestre;
+    int notaSegundoTrimestre;
+    int notaTercerTrimestre;
+    int notaMedia;
+
+    public Notas(String nombreAsignatura, int notaPrimerTrimestre, int notaSegundoTrimestre, int notaTercerTrimestre, int notaMedia) {
+        this.nombreAsignatura = nombreAsignatura;
+        this.notaPrimerTrimestre = notaPrimerTrimestre;
+        this.notaSegundoTrimestre = notaSegundoTrimestre;
+        this.notaTercerTrimestre = notaTercerTrimestre;
+        this.notaMedia = notaMedia;
+    }
+    int contador = 0;
+    public void trimestresAprobados(){
+        contador = 0;
+        if (notaPrimerTrimestre >= 5){
+            contador = contador+1;
+        }
+        if (notaSegundoTrimestre >= 5){
+            contador = contador+1;
+        }
+        if (notaTercerTrimestre >= 5){
+            contador = contador+1;
+        }
+        System.out.println("Ha aprobado " +contador+ " trimestres.");
+    }
+
+    public void notaMediaFinal(){
+        notaMedia = (notaPrimerTrimestre+notaSegundoTrimestre+notaTercerTrimestre)/3;
+        System.out.println("La nota media es " +notaMedia);
+    }
 }
